d5e72ae8ffedfc2172d448cbcf110ce05025d271
Your Name <you@example.com>
2022-03-18 23:41:10 -0300

    Response

---
+++ JS/zener.js
@@ -0,0 +1,11 @@
+
+
+
+/* function h1Before(){
+
+    const h1WindowSmall = document.createElement("h1")
+    h1WindowSmall.innerHTML = 'Análise de Qualidade de Energia'
+    document.getElementById("#Divh1Img").appendChild(h1WindowSmall)
+    alert()
+}
+setInterval(h1Before,1000) */
