0cbca09a40cbec824d984f4e4444bfeb6e253108
Your Name <you@example.com>
2022-03-23 08:49:08 -0300

    LinkWhats

--- JS/zener.js
+++ JS/zener.js
@@ -1,3 +1,12 @@
 
+function linkWhats() {
 
-
+    const yesNo = confirm("Você será redirecionado para o nosso whatsapp da Zener Engenharia.")
+    if(yesNo == true) {
+        
+    } else {
+       const ancora = document.getElementById("ancora")
+       ancora.setAttribute("href","#")
+       ancora.setAttribute("target","_self")
+    }
+}
