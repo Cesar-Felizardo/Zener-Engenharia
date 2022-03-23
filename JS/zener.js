95bfb0bf04742c79bb80dbcbc4e262bff7a8cd80
Your Name <you@example.com>
2022-03-23 10:02:32 -0300

    Reflesh

--- JS/zener.js
+++ JS/zener.js
@@ -1,12 +1,12 @@
 
 function linkWhats() {
 
-    const yesNo = confirm("Você será redirecionado para o nosso whatsapp da Zener Engenharia.")
+    const yesNo = confirm("Você será redirecionado para nosso whatsapp.")
     if(yesNo == true) {
         
     } else {
        const ancora = document.getElementById("ancora")
-       ancora.setAttribute("href","#")
+       ancora.setAttribute("href","index.html")
        ancora.setAttribute("target","_self")
     }
 }
