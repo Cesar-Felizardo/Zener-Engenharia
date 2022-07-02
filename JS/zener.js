4953245ecd3a5211d9e52f407b4f215e418ec17a
Your Name <you@example.com>
2022-07-02 12:03:55 -0300

    ultima versão zener

--- JS/zener.js
+++ JS/zener.js
@@ -1,5 +1,5 @@
 
-function linkWhats() {
+/* function linkWhats() {
 
     const yesNo = confirm("Você será redirecionado para nosso whatsapp.")
     if(yesNo == true) {
@@ -9,4 +9,61 @@ function linkWhats() {
        ancora.setAttribute("href","index.html")
        ancora.setAttribute("target","_self")
     }
+} */
+
+
+/* window.addEventListener("load", start_animate)
+const whatsappImg = document.getElementById("whatsappImg")
+const animate_whats = document.querySelector("animate__animated animate__shakeX")
+
+function mover_whats() {
+    alert
+    
 }
+
+function start_animate() {
+    setInterval(() =>{
+        mover_whats()
+    },2000)
+} */
+
+window.addEventListener('load', welcome)
+function welcome() {
+    var data = new Date();
+    var hora = data.getHours()
+    if(hora < 12) {
+        alert('Ola, Bom dia! Seja bem vindo ao nosso site!')
+    } else if (hora <= 18) {
+        alert('Ola, Boa tarde! Seja bem vindo ao nosso site!')
+    } else if (hora <= 23) {
+        alert('Ola, Boa noite! Seja bem vindo ao nosso site!')
+    } else if (hora <= 5) {
+        alert('Ola, Boa madrugada! Seja bem vindo ao nosso site!')
+    } 
+}
+window.addEventListener('load', start_OnOff)
+const phone_more = document.querySelector('phonesMore')
+
+function lightOn() {
+    
+}
+
+function start_OnOff() {
+    setInterval(function() {
+        lightOn()
+    },2000)
+}
+// Obtém a data/hora atual
+/* var data = new Date(); */
+
+// Guarda cada pedaço em uma variável
+/* var dia     = data.getDate(); */           // 1-31
+/* var dia_sem = data.getDay(); */            // 0-6 (zero=domingo)
+/* var mes     = data.getMonth(); */          // 0-11 (zero=janeiro)
+/* var ano2    = data.getYear();        */    // 2 dígitos
+/* var ano4    = data.getFullYear();   */     // 4 dígitos
+/* var hora    = data.getHours();       */    // 0-23
+/* var min     = data.getMinutes();  */       // 0-59
+/* var seg     = data.getSeconds(); */        // 0-59
+/* var mseg    = data.getMilliseconds(); */   // 0-999
+/* var tz      = data.getTimezoneOffset(); */ // em minutos
